aa63a7111779214b54737a8ab5e3403caf7ada7f
Akash Sahu <akashsahu793@gmail.com>
2024-09-04 00:35:20 +0530

    added Services-component
    6:28 -> all good

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from "./components/Benefits";
 import Collabration from "./components/Collabration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collabration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
